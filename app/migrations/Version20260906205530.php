581b961e05d1f5e8ef53821b62f0d1eea88ea332
Jakub <kubaryszka@wp.pl>
2026-09-06 23:47:56 +0200

    commit numer 6

--- app/migrations/Version20260906205530.php
+++ app/migrations/Version20260906205530.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260905212738 extends AbstractMigration
+final class Version20260906205530 extends AbstractMigration
 {
     public function getDescription(): string
     {
